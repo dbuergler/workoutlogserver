89a55f542f0821341d922ce46c525a170cc63386
Daniel Buergler <daniel.jbuergler@gmail.com>
2021-01-22 22:29:11 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 let express = require('express');
 let app = express();
-let log = require('./controllers/logcontroller')
+let log = require('./controllers/logcontroller');
+let user = require('./controllers/userlogcontroller');
 let sequelize = require('./db');
 
-app.use('/log', log)
+app.use(express.json());
+
+app.use('/log', log);
+app.use('/user', user);
+
 sequelize.sync();
+
 //sequelize.sync({force: true})
 
 
